b70d86b378784eefcc782b023157eaa413b3015c
Devon Robinson <drob678@gmail.com>
2018-08-10 19:53:03 -0500

    shared_socket

---
+++ shared_socket/client.cpp
@@ -0,0 +1,46 @@
+#include <boots/asio/io_service.hpp>
+#include <boost/asio/write.hpp>
+#include <boost/asio/buffer.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <array>
+#include <string>
+#include <iostream>
+
+using namespace boost::asio;
+using namespace boost::asio::ip;
+
+int main()
+{
+    boost::asio::io_service aios;
+
+    boost::asio::ip::tcp::resolver resolver(aios);
+    boost::asio::ip::tcp::resolver::iterator endpoint = resolver.resolve(
+        boost::asio::ip::tcp::resolver::query("localhost",2222));
+
+    boost::asio::ip::tcp::socket:: socket(aios);
+    boost::asio::conncet(socket, endpoint);
+
+    for(;;)
+    {
+        std::array<char,256>buf{};
+        boost::system::error_code error;
+        boost::asio::write((socket, boost::asio::buffer(std::string("hi")));
+        size_t len = socket.read_some(boost::asio::buffer(buf), error);
+
+        if(error == boost::asio::error::eof)
+        {
+            std::cout << "server close socket\n";
+            break;
+        }
+        else if(error)
+        {
+            std::cout << error.message() << "\n";
+        }
+
+        std::cout.write(buf.data(), len);
+        std::cout << '|':
+    }
+    std::cout << std::endl;
+
+    return EXIT_SUCCESS;
+}
